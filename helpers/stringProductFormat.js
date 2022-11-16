cf211d80a5596e3fe08f1f5eaabfb1e97d6e97ad
Dmitry <d.o.blinov@mail.ru>
2022-11-16 22:33:07 +0600

    added string format

---
+++ helpers/stringProductFormat.js
@@ -0,0 +1,15 @@
+export default function stringProductFormat (amount) {
+  if (Number(amount.toString()
+    .slice(-1)) === 1) {
+    return 'товар'
+  }
+  if (Number(amount.toString() >= 11 && Number(amount.toString() <= 20))) {
+    return 'товаров'
+  }
+  if (Number(amount.toString()
+    .slice(-1)) >= 2 && Number(amount.toString()
+    .slice(-1)) <= 4) {
+    return 'товара'
+  }
+  return 'товаров'
+}
