32fad8a9e94a251e8351b9e3fb3f5de6cebdf0a2
Dmitry <d.o.blinov@mail.ru>
2022-11-18 01:14:47 +0600

    created component colors for product

---
+++ components/BaseColor.vue
@@ -0,0 +1,58 @@
+<template>
+  <ul class="colors">
+    <li v-for="item in colors" :key="item.id" class="colors__item" :class="{'colors__active': item.id === currentColor}">
+      <label class="colors__label">
+        <input
+          v-model="computedColor"
+          class="colors__radio sr-only"
+          :type="type"
+          :value="item.id"
+        >
+        <span
+          class="colors__value"
+          :class="{'value--black': item.title === 'white'}"
+          :style="{backgroundColor: item.code}"
+        />
+      </label>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'BaseColor',
+  props: {
+    colors: {
+      type: Object,
+      default: () => {}
+    },
+    currentColor: {
+      type: Number,
+      default: 0
+    },
+    type: {
+      default: 'radio',
+      type: String
+    }
+  },
+  computed: {
+    computedColor: {
+      get () {
+        return this.currentColor
+      },
+      set (value) {
+        this.$emit('update:currentColor', value)
+      }
+    }
+  }
+}
+</script>
+<style scoped>
+.colors__active {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: 1px solid black;
+  border-radius: 50%;
+}
+</style>
