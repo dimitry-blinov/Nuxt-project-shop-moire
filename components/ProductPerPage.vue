20f59666ffab213de6015d2859076ddd9639636f
Dmitry <d.o.blinov@mail.ru>
2022-11-18 00:35:29 +0600

    corrected the names, added switching to 1 page when changing the number of products

--- components/ProductPerPage.vue
+++ components/ProductPerPage.vue
@@ -4,13 +4,13 @@
       Количество товаров на странице:
     </span>
     <label class="form__label form__label--small form__label--select">
-      <select v-model="perPageProduct" class="form__select" name="category">
+      <select v-model="computedPerPage" class="form__select" name="category">
         <option
-          v-for="item in limits"
-          :key="item.id"
-          :value="item.value"
+          v-for="(value, index) in values"
+          :key="index"
+          :value="value"
         >
-          {{ item.value }}
+          {{ value }}
         </option>
       </select>
     </label>
@@ -18,45 +18,27 @@
 </template>
 
 <script>
-// TODO: Поправить нейминг методов, пропсов
 export default {
   name: 'ProductDisplaying',
   props: {
-    page: {
-      type: Number,
-      default: 1
-    },
     perPage: {
       default: 3,
       type: Number
     },
-    limits: {
-      default: () => [
-        {
-          id: 1,
-          value: 3
-        },
-        {
-          id: 2,
-          value: 6
-        },
-        {
-          id: 3,
-          value: 9
-        }
-      ],
+    values: {
+      default: () => [3, 6, 9, 12, 15],
       type: Array
     }
   },
   computed: {
-    perPageProduct: {
+    computedPerPage: {
       get () {
         return this.perPage
       },
       set (limit) {
         this.$router.push({
           query: {
-            page: this.page,
+            page: 1,
             limit
           }
         })
