236b9ca7a3de5fb3178fafa5188b3ea090863807
dmitryblinov <d.o.blinov@mail.ru>
2022-11-23 22:08:11 +0500

    added baseURL for axios

--- nuxt.config.js
+++ nuxt.config.js
@@ -49,6 +49,9 @@ export default {
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: ['@nuxtjs/axios'],
+  axios: {
+    baseURL: 'https://vue-moire.skillbox.cc'
+  },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {}
